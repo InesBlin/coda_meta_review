f7f51597da8c944fce0a83fc55e0ce52a3ebc73b
Ines Blin <iblin431@outlook.fr>
2024-04-12 18:54:39 +0200

    improving the meta-review automatic generation

--- src/meta_review.py
+++ src/meta_review.py
@@ -1,11 +1,36 @@
 import os
 import re
+import yaml
+import json
 import pandas as pd
 import numpy as np
 from copy import deepcopy
 from src.pipeline import Pipeline
+import plotly.graph_objs as go
 from jinja2 import Environment, FileSystemLoader
 import plotly.express as px
+
+fig1 = go.Figure(data=go.Scatter(x=[1, 2, 3], y=[4, 1, 2]))
+fig1.update_layout(title='Figure 1')
+
+fig2 = go.Figure(data=go.Bar(x=[1, 2, 3], y=[4, 1, 2]))
+fig2.update_layout(title='Figure 2')
+
+fig3 = go.Figure(data=go.Pie(labels=['A', 'B', 'C'], values=[4, 3, 2]))
+fig3.update_layout(title='Figure 3')
+
+def custom_enumerate(iterable, start=1):
+    return enumerate(iterable, start)
+
+def load_yaml_file(filename):
+    with open(filename, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+def load_json_file(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
 
 def categorize_es(x):
     """ categorize effect size """
@@ -20,6 +45,13 @@ def categorize_pval(x):
     if x < 0.05:
         return "statistically significant"
     return "not statistically significant"
+
+def replace_cite_id(input_text, dico):
+    pattern = r'<cite (.*?)>'
+    def replace_with_dict(match):
+        key = match.group(1) 
+        return f"[{dico.get(key, '')}]"
+    return re.sub(pattern, replace_with_dict, input_text)
 
 class MetaReview:
     """ Main class for generating the meta-review
@@ -52,6 +84,11 @@ class MetaReview:
         """ Init main templates/variables """
         self.data = args["data"]
         self.h = args["hypothesis"]
+        self.config = load_yaml_file(args["config"])
+        self.references = load_json_file(args["references"])
+        self.id_ref_to_nb = {x["id"]: i+1 for i, x in enumerate(self.references)}
+        self.config = {k: replace_cite_id(v, self.id_ref_to_nb) if isinstance(v, str) else v for k, v in self.config.items()}
+        self.structure = load_yaml_file(args["structure"])
         self.key_qualifier = [x for x in self.h.keys() if x.endswith("qualifier")][0]
         self.type_h = self.get_type_hypothesis()
 
@@ -73,6 +110,7 @@ class MetaReview:
         self.columns = ["studyName", "effectSize", "effectSizeSampleSize"]
 
         env = Environment(loader=FileSystemLoader('.'))
+        env.filters['custom_enumerate'] = custom_enumerate
         self.template = env.get_template(args["report_template"])
 
     def get_type_hypothesis(self):
@@ -169,42 +207,56 @@ class MetaReview:
         html_review = self.template.render(**kwargs)
         return html_review
     
-    def __call__(self, save_folder):
-        """ Run meta-analysis and produce meta-review """
-        data_ma = self.pipeline.get_data_meta_analysis(self.data)
-        data_ma.to_csv(os.path.join(save_folder, "data_ma_init.csv"))
-        output = self.pipeline(data=self.data)
-        ma_res, refs = output["results_rma"], output["refs"]
-        output["data"].to_csv(os.path.join(save_folder, "data_ma_pre_ma.csv"))
-        es, pval = ma_res['b'][0][0], ma_res["pval"][0]
-
-        df_map = data_ma.groupby("country").agg({"observation": "nunique"}).reset_index()
+    def get_figures(self, data):
+        """ Get figures to be displayed """
+        df_map =data.groupby("country").agg({"observation": "nunique"}).reset_index()
         fig_map = px.choropleth(
             df_map, locations="country",
             color="observation", # lifeExp is a column of gapminder
             # hover_name="country", # column to add to hover information
             color_continuous_scale=px.colors.sequential.Plasma)
-        fig_map.write_html(os.path.join(save_folder, "fig_map.html"))
 
-        fig_hist_year = px.histogram(data_ma, x="paperYearDOI", nbins=50)
-        fig_hist_year.write_html(os.path.join(save_folder, "fig_hist_year.html"))
+        fig_hist_year = px.histogram(data, x="paperYearDOI", nbins=50)
+        return {"fig_study_provenance": fig_map.to_html(), "fig_study_year": fig_hist_year.to_html()}
+    
+    def __call__(self, save_folder):
+        """ Run meta-analysis and produce meta-review """
+        data_ma = self.pipeline.get_data_meta_analysis(self.data)
+        data_ma.to_csv(os.path.join(save_folder, "data_ma_init.csv"))
+        output = self.pipeline(data=self.data, es_measure=self.config["es_measure"], method=self.config["method_mv"])
+        ma_res, refs = output["results_rma"], output["refs"]
+        output["data"].to_csv(os.path.join(save_folder, "data_ma_pre_ma.csv"))
+        es, pval = ma_res['b'][0][0], ma_res["pval"][0]
 
         conclude_hypothesis = self.verify_hypothesis(es=es, pval=pval)
-        title = "Meta-Review"
+
+        type_es = "standardized mean difference" if self.config["es_measure"] == "d" else "raw correlation coefficient"
+        name_es = "Cohen" if self.config["es_measure"] == "d" else "Pearson"
 
         args = dict(
             hypothesis=self.text_h, name=self.h['giv1'], info=self.h,
-            data_ma_shape=format(data_ma.shape[0], ','),
-            columns=self.columns, data_ma=data_ma[self.columns], ma_res_call=ma_res["call"],
-            es=round(es, 2), categorize_es=categorize_es(es), title=title,
+            data_ma_shape=format(output["data"].shape[0], ','),
+            data_ma_es_nb=format(output["data"].shape[0], ','),
+            data_ma_study_nb=format(output["data"].study.unique().shape[0], ','),
+            data_ma_country_nb=format(output["data"].country.unique().shape[0], ','),
+            columns=self.columns, data_ma=output["data"][self.columns], ma_res_call=ma_res["call"],
+            es=round(es, 2), categorize_es=categorize_es(es), 
             pval=pval, categorize_pval=categorize_pval(pval),
-            conclude_hypothesis=conclude_hypothesis)
+            conclude_hypothesis=conclude_hypothesis,
+            references=self.references,
+            type_es=type_es, name_es=name_es)
+        args.update(self.config)
+        args.update(self.structure)
+        args.update(self.get_figures(data=output["data"]))
 
         html_review = self.generate_html_review(**args)
         with open(os.path.join(save_folder, "report.html"), "w", encoding="utf-8") as file:
             file.write(html_review)
 
 ARGS = {
+    'config': 'meta_review/config.yaml',
+    'structure': 'meta_review/structure.yaml',
+    'references': 'meta_review/references.json',
     "data": pd.read_csv("./data/observationData.csv", index_col=0),
     "cached": {
         "study_moderators": "./data/moderators/study_moderators.csv",
@@ -214,9 +266,14 @@ ARGS = {
         "variable_moderators": "./data/moderators/variable_moderators.csv"
     },
     "report_template": 'meta_review/report_template.html',
+    # "hypothesis": {
+    #     "giv1": "group_size", "siv1": "decision maker", "sivv1": "individual",
+    #     "giv2": "group_size", "siv2": "group size level", "sivv2": "high",
+    #     'reg_qualifier': 'higher'
+    # }
     "hypothesis": {
-        "giv1": "group_size", "siv1": "decision maker", "sivv1": "individual",
-        "giv2": "group_size", "siv2": "group size level", "sivv2": "high",
+        "giv1": "gender", "siv1": "gender", "sivv1": "female",
+        "giv2": "gender", "siv2": "gender", "sivv2": "male",
         'reg_qualifier': 'higher'
     }
 }
