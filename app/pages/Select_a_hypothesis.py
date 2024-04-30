c17a03da42183f4c3bacdbff9a85d541c3542ff6
Ines Blin <iblin431@outlook.fr>
2024-04-30 18:17:10 +0200

    fixed hypotheses being regenerated everytime + minor changes + still problem with rpy2

--- app/pages/Select_a_hypothesis.py
+++ app/pages/Select_a_hypothesis.py
@@ -1,67 +1,66 @@
 # -*- coding: utf-8 -*-
 """ Expert's recommendation """
+import json
 import pandas as pd
-from src.meta_review import MetaReview
-import streamlit as st
+from src.meta_review import MetaReview, load_json_file
 from src.hg.literature import LiteratureHypothesisGenerator
 from src.hg.llm import LLMHypothesisGenerator
+import streamlit as st
+from datetime import datetime
 
 
 st.title("Select a hypothesis")
 st.write("#")
 st.write("We will help you pick a hypothesis to explore with the CoDa databank.")
-hypothesis_selector = st.radio(
-    "How would you like to select your hypothesis?",
-    ["From literature", "LLM-Based", "LP-Based"],
-    index=None,
-)
 
-st.write("You selected:", hypothesis_selector)
+with st.form("hypotheses_params_form"):
+    hypotheses_selector = st.radio(
+        "How would you like to select your hypothesis?",
+        ["From literature", "LLM-Based", "LP-Based"],
+        index=None, key="hypotheses_selector"
+    )
+    top_k = st.number_input('How many hypotheses would you like to generate?', value=3, key="top_k")
+    submit_hp = st.form_submit_button("Generate Hypotheses Params")
 
-top_k = st.number_input('How many hypotheses would you like to generate?', value=3)
-hypotheses = None
+if submit_hp:
+    st.session_state["submit_hp"] = True
 
-if hypothesis_selector == "From literature":
-    giv = None
-    hg = LiteratureHypothesisGenerator()
-    hypotheses = hg(giv=giv, top_k=int(top_k))
-
-if hypothesis_selector == "LLM-Based":
-    giv = None
-    data = "./data/prompt_data_based.csv"
-    scoring_selector = st.radio(
-        "How would you like to rank your hypotheses?",
-        ["random", "frequency", "entropy"],
-        index=None)
-    if scoring_selector:
-        hg = LLMHypothesisGenerator(data=data, scoring=scoring_selector)
+if st.session_state.get("submit_hp") and not st.session_state.get("hypotheses"):
+    if st.session_state.hypotheses_selector == "From literature":
+        giv = None
+        hg = LiteratureHypothesisGenerator()
         hypotheses = hg(giv=giv, top_k=int(top_k))
+        st.session_state["hypotheses"] = hypotheses
+    if hypotheses_selector == "LLM-Based":
+        giv = None
+        data = "./data/prompt_data_based.csv"
+        scoring_selector = st.radio(
+            "How would you like to rank your hypotheses?",
+            ["random", "frequency", "entropy"],
+            index=None)
+        if scoring_selector:
+            hg = LLMHypothesisGenerator(data=data, scoring=scoring_selector)
+            hypotheses = hg(giv=giv, top_k=int(top_k))
+            st.session_state["hypotheses"] = hypotheses
 
-if hypothesis_selector == "LP-Based":
-    st.warning("Not yet implemented")
+    if hypotheses_selector == "LP-Based":
+        st.warning("Not yet implemented")
 
-if hypotheses:
-    hypothesis = st.radio(
-        "Which hypothesis would you like to explore?",
-        hypotheses,
-        index=None)
-    if hypothesis:
-        st.write(hypothesis)
-        # ARGS = {
-        #     'template_folder': 'meta_review/templates',
-        #     'config': 'meta_review/config.yaml',
-        #     'structure': 'meta_review/templates/structure.yaml',
-        #     'references': 'meta_review/templates/references.json',
-        #     "data": pd.read_csv("./data/observationData.csv", index_col=0),
-        #     "cached": {
-        #         "study_moderators": "./data/moderators/study_moderators.csv",
-        #         "country_moderators": "./data/moderators/country_moderators.csv",
-        #         "simple_country_moderators": "./data/moderators/simple_country_moderators.csv",
-        #         "complex_country_moderators": "./data/moderators/complex_country_moderators.csv",
-        #         "variable_moderators": "./data/moderators/variable_moderators.csv"
-        #     },
-        #     "report_template": 'report_template.html',
-        # }
-        # ARGS.update(hypothesis)
-        # meta_review = MetaReview(**ARGS)
-        # meta_review(save_folder="./app/meta_review")
+if st.session_state.get("hypotheses"):
+    with st.form("hypotheses_form"):
+        hypothesis_selector = st.radio(
+            "Which hypothesis would you like to explore?",
+            st.session_state.get("hypotheses"),
+            index=None, key="hypothesis_selector"
+        )
+        submit_h = st.form_submit_button("Submit Hypothesis")
+    
+    if submit_h:
+        st.session_state["submit_h"] = True
+
+if st.session_state.get("submit_h") and st.session_state.get("hypothesis_selector"):
+    config = load_json_file(filename="./src/configs/meta_review_base.json")
+    config.update({"hypothesis": st.session_state.get("hypothesis_selector")})
+    meta_review = MetaReview(config=config)
+    meta_review(save_folder="test/")
+
--- src/meta_review.py
+++ src/meta_review.py
@@ -6,6 +6,7 @@ import os
 import re
 import json
 import math
+from typing import Union, Dict
 from copy import deepcopy
 import yaml
 import click
@@ -19,17 +20,20 @@ def custom_enumerate(iterable, start=1):
     """ Custom enumerate for HTML """
     return enumerate(iterable, start)
 
+
 def load_yaml_file(filename):
     """ Self explanatory """
     with open(filename, 'r', encoding='utf-8') as file:
         data = yaml.safe_load(file)
     return data
 
+
 def load_json_file(filename):
     """ Self explanatory """
     with open(filename, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
+
 
 def categorize_es(x):
     """ categorize effect size """
@@ -39,19 +43,22 @@ def categorize_es(x):
         return "small"
     return "null"
 
+
 def categorize_pval(x):
     """ categorize pvalue """
     if x < 0.05:
         return "statistically significant"
     return "not statistically significant"
 
+
 def replace_cite_id(input_text, dico):
-    """ In text, replace ~\cite{} with [nb] """
+    """ In text, replace ~\\cite{} with [nb] """
     pattern = r'<cite (.*?)>'
     def replace_with_dict(match):
-        key = match.group(1) 
+        key = match.group(1)
         return f"[{dico.get(key, '')}]"
     return re.sub(pattern, replace_with_dict, input_text)
+
 
 class MetaReview:
     """ Main class for generating the meta-review
@@ -80,15 +87,24 @@ class MetaReview:
     [NB]: for now focus on 1-
     """
 
-    def __init__(self, **args):
+    def __init__(self, config: Union[Dict, str]):
         """ Init main templates/variables """
-        self.data = args["data"]
-        self.h = args["hypothesis"]
-        self.config = load_yaml_file(args["config"])
-        self.references = load_json_file(args["references"])
+
+        if isinstance(config, str):
+            config = load_json_file(filename=config)
+
+        if isinstance(config["data"], str):
+            config["data"] = pd.read_csv(config["data"], index_col=0)
+
+
+        self.data = config["data"]
+        self.h = config["hypothesis"]
+        self.config = load_yaml_file(config["config"])
+        self.references = load_json_file(config["references"])
         self.id_ref_to_nb = {x["id"]: i+1 for i, x in enumerate(self.references)}
-        self.config = {k: replace_cite_id(v, self.id_ref_to_nb) if isinstance(v, str) else v for k, v in self.config.items()}
-        self.structure = load_yaml_file(args["structure"])
+        self.config = {k: replace_cite_id(v, self.id_ref_to_nb) \
+            if isinstance(v, str) else v for k, v in self.config.items()}
+        self.structure = load_yaml_file(config["structure"])
         self.key_qualifier = [x for x in self.h.keys() if x.endswith("qualifier")][0]
         self.type_h = self.get_type_hypothesis()
 
@@ -106,12 +122,12 @@ class MetaReview:
 
         self.pipeline = Pipeline(
             giv1=self.h['giv1'], siv1=self.h['siv1'], sivv1=self.h['sivv1'],
-            giv2=self.h['giv2'], siv2=self.h['siv2'], sivv2=self.h['sivv2'], **args.get("cached"))
+            giv2=self.h['giv2'], siv2=self.h['siv2'], sivv2=self.h['sivv2'], **config.get("cached"))
         self.columns = ["studyName", "effectSize", "effectSizeSampleSize"]
 
-        env = Environment(loader=FileSystemLoader(args['template_folder']))
+        env = Environment(loader=FileSystemLoader(config['template_folder']))
         env.filters['custom_enumerate'] = custom_enumerate
-        self.template = env.get_template(args["report_template"])
+        self.template = env.get_template(config["report_template"])
 
         self.i2_threshold = 25
         self.tau2_threshold = 0.5
@@ -209,7 +225,7 @@ class MetaReview:
         """ self explanatory """
         html_review = self.template.render(**kwargs)
         return html_review
-    
+
     def get_figures(self, data):
         """ Get figures to be displayed """
         df_map =data.groupby("country").agg({"observation": "nunique"}).reset_index()
@@ -220,64 +236,69 @@ class MetaReview:
             color_continuous_scale=px.colors.sequential.Plasma)
 
         fig_hist_year = px.histogram(data, x="paperYearDOI", nbins=50)
-        return {"fig_study_provenance": fig_map.to_html(), "fig_study_year": fig_hist_year.to_html()}
-    
+        return {
+            "fig_study_provenance": fig_map.to_html(),
+            "fig_study_year": fig_hist_year.to_html()}
+
     def get_i2_description(self, i2):
         """ Describe heterogeneity across studies """
         if i2 >= self.i2_threshold:
-            return f"There is a high heterogeneity across studies (I2 = {round(i2, 2)}%), which means that the studies cannot be considered studies of the same population. We suggest that a finer-grained subgroup analysis might be worthwhile."
-        return f"There is a low heterogeneity across studies (I2 = {round(i2, 2)}%), which means that the studies can be considered studies of the same population. "
-    
+            return f"There is a high heterogeneity across studies (I2 = {round(i2, 2)}%), " + \
+                "which means that the studies cannot be considered studies of the same " + \
+                    "population. We suggest that a finer-grained subgroup analysis might " + \
+                        "be worthwhile."
+        return f"There is a low heterogeneity across studies (I2 = {round(i2, 2)}%), " + \
+            "which means that the studies can be considered studies of the same population. "
+
     def get_tau2_description(self, tau2):
         """ Describe dispersion of time effect sizes """
         how = "low" if tau2 <= self.tau2_threshold else "high"
-        return f"There is a {how} dispersion of time effect sizes (tau = {round(math.sqrt(tau2), 2)}, T2 = {round(tau2, 2)})."
-    
+        return f"There is a {how} dispersion of time effect sizes " + \
+            f"(tau = {round(math.sqrt(tau2), 2)}, T2 = {round(tau2, 2)})."
+
     def format_moderator(self):
         """ Format moderator for meta-analysis """
         if self.type_h == 'regular':
             return None
         return {self.h['type_mod']: [self.h['mod']]}
-    
+
     def __call__(self, save_folder):
         """ Run meta-analysis and produce meta-review """
-        data_ma = self.pipeline.get_data_meta_analysis(self.data)
-        data_ma.to_csv(os.path.join(save_folder, "data_ma_init.csv"))
-        output = self.pipeline(data=self.data, es_measure=self.config["es_measure"], method=self.config["method_mv"], mods=self.format_moderator())
+        output = self.pipeline(
+            data=self.data, es_measure=self.config["es_measure"],
+            method=self.config["method_mv"], mods=self.format_moderator())
         ma_res, refs = output["results_rma"], output["refs"]
-        output["data"].to_csv(os.path.join(save_folder, "data_ma_pre_ma.csv"))
         es, pval, i2 = ma_res['b'][-1][0], ma_res["pval"][-1], ma_res['I2'][-1]
         ci_lb, ci_ub = ma_res["ci.lb"][-1], ma_res["ci.ub"][-1]
-        tau2 = ma_res['tau2'][-1]
 
-        conclude_hypothesis = self.verify_hypothesis(es=es, pval=pval)
-
-        type_es = "standardized mean difference" if self.config["es_measure"] == "d" else "raw correlation coefficient"
+        type_es = "standardized mean difference" \
+            if self.config["es_measure"] == "d" else "raw correlation coefficient"
         name_es = "Cohen" if self.config["es_measure"] == "d" else "Pearson"
         ex_mod_read = [x for x in ma_res['df_info'].info_treatment.values if x != 'intrcpt'][0]
 
-        args = dict(
-            hypothesis=self.text_h, name=self.h['giv1'], info=self.h,
-            data_ma_shape=format(output["data"].shape[0], ','),
-            data_ma_es_nb=format(output["data"].shape[0], ','),
-            data_ma_study_nb=format(output["data"].study.unique().shape[0], ','),
-            data_ma_country_nb=format(output["data"].country.unique().shape[0], ','),
-            columns=self.columns, data_ma_id='data_ma_id', data_ma=output["data"][self.columns], ma_res_call=ma_res["call"],
-            es=round(es, 2), categorize_es=categorize_es(es), 
-            pval=round(pval, 3), categorize_pval=categorize_pval(pval),
-            conclude_hypothesis=conclude_hypothesis,
-            references=self.references,
-            type_es=type_es, name_es=name_es,
-            i2_description=self.get_i2_description(i2=i2),
-            tau2_description=self.get_tau2_description(tau2=tau2),
-            ci_lb=round(ci_lb, 3), ci_ub=round(ci_ub, 3),
-            df_info_id="df_info_id", df_info=ma_res['df_info'],
-            type_h=self.type_h, mod=self.h.get('mod'),
-            type_mod=self.type_h.split('_', maxsplit=1)[0],
-            ref_mod=refs[self.h.get('mod')],
-            ex_mod_read=ex_mod_read,
-            ex_mod_val=ex_mod_read.replace(self.h.get('mod'), '') 
-            )
+        args = {
+            "hypothesis": self.text_h, "name": self.h['giv1'], "info": self.h,
+            "data_ma_shape": format(output["data"].shape[0], ','),
+            "data_ma_es_nb": format(output["data"].shape[0], ','),
+            "data_ma_study_nb": format(output["data"].study.unique().shape[0], ','),
+            "data_ma_country_nb": format(output["data"].country.unique().shape[0], ','),
+            "columns": self.columns, "data_ma_id": 'data_ma_id',
+            "data_ma": output["data"][self.columns], "ma_res_call": ma_res["call"],
+            "es": round(es, 2), "categorize_es": categorize_es(es),
+            "pval": round(pval, 3), "categorize_pval": categorize_pval(pval),
+            "conclude_hypothesis": self.verify_hypothesis(es=es, pval=pval),
+            "references": self.references,
+            "type_es": type_es, "name_es": name_es,
+            "i2_description": self.get_i2_description(i2=i2),
+            "tau2_description": self.get_tau2_description(tau2=ma_res['tau2'][-1]),
+            "ci_lb": round(ci_lb, 3), "ci_ub": round(ci_ub, 3),
+            "df_info_id": "df_info_id", "df_info": ma_res['df_info'],
+            "type_h": self.type_h, "mod": self.h.get('mod'),
+            "type_mod": self.type_h.split('_', maxsplit=1)[0],
+            "ref_mod": refs[self.h.get('mod')] if self.h.get('mod') else None,
+            "ex_mod_read": ex_mod_read,
+            "ex_mod_val": ex_mod_read.replace(self.h.get('mod'), '') if self.h.get('mod') else None
+            }
         args.update(self.config)
         args.update(self.structure)
         args.update(self.get_figures(data=output["data"]))
@@ -286,51 +307,13 @@ class MetaReview:
         with open(os.path.join(save_folder, "report.html"), "w", encoding="utf-8") as file:
             file.write(html_review)
 
-ARGS = {
-    'template_folder': 'meta_review/templates',
-    'config': 'meta_review/config.yaml',
-    'structure': 'meta_review/templates/structure.yaml',
-    'references': 'meta_review/templates/references.json',
-    "data": pd.read_csv("./data/observationData.csv", index_col=0),
-    "cached": {
-        "study_moderators": "./data/moderators/study_moderators.csv",
-        "country_moderators": "./data/moderators/country_moderators.csv",
-        "simple_country_moderators": "./data/moderators/simple_country_moderators.csv",
-        "complex_country_moderators": "./data/moderators/complex_country_moderators.csv",
-        "variable_moderators": "./data/moderators/variable_moderators.csv"
-    },
-    "report_template": 'report_template.html',
-    # REGULAR hypothesis
-    # "hypothesis": {
-    #     "giv1": "gender", "siv1": "gender", "sivv1": "female",
-    #     "giv2": "gender", "siv2": "gender", "sivv2": "male",
-    #     'reg_qualifier': 'higher'
-    # },
-    # NUMERICAL hypothesis
-    # "hypothesis": {
-    #     "giv1": "gender", "siv1": "gender", "sivv1": "female",
-    #     "giv2": "gender", "siv2": "gender", "sivv2": "male",
-    #     'num_qualifier': 'higher', 'mod_qualifier': 'higher',
-    #     'mod': 'studyKindex', 'type_mod': 'study'
-    # }
-    # CATEGORICAL hypothesis
-    "hypothesis": {
-        "giv1": "gender", "siv1": "gender", "sivv1": "female",
-        "giv2": "gender", "siv2": "gender", "sivv2": "male",
-        'cat_qualifier': 'higher', 'mod_qualifier': 'higher',
-        # 'mod': 'studyOneShot', 'type_mod': 'study',
-        # 'mod1': 'Repeated', 'mod2': 'One-shot'
-        'mod': 'country', 'type_mod': 'study',
-        'mod1': 'FRA', 'mod2': 'KOR'
-    }
-}
-
 
 @click.command()
+@click.argument("config")
 @click.argument("save_folder")
-def main(save_folder):
+def main(config, save_folder):
     """ Main meta review generation """
-    meta_review = MetaReview(**ARGS)
+    meta_review = MetaReview(config=config)
     meta_review(save_folder=save_folder)
 
 
